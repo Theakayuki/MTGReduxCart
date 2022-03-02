ed92733cd2cdcca6626f62bef83ee2343348a323
Theakayuki <58452606+Theakayuki@users.noreply.github.com>
2022-03-02 16:58:25 -0600

    added more selection

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -36,6 +36,27 @@ const Products = (props) => {
                     addItem={addItemToCart}
                     img={'CrimsonGiftSet'}
                 />
+                <ProductItem
+                    title='MTG: Crimson Vow Mouse Pad'
+                    price={9.99}
+                    description='Crimson Vow Mouse Pad, 10" x 8" x 1.5" (1.5" thick) with rubber backing'
+                    addItem={addItemToCart}
+                    img={'VowMousePad'}
+                />
+                <ProductItem
+                    title='MTG: Vampire and Bats Tee'
+                    price={24.99}
+                    description='Crimson Vow theme tee with Vampire and Bats'
+                    addItem={addItemToCart}
+                    img={'Vampires-and-Bats'}
+                />
+                <ProductItem
+                    title='MTG: Bride Tee'
+                    price={24.99}
+                    description='Crimson Vow theme tee featuring the Bride'
+                    addItem={addItemToCart}
+                    img={'VOW-Olivia-Crimson-BrideTee'}
+                />
             </ul>
         </section>
     );
